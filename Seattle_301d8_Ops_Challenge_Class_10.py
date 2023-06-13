b84fdc1440bbd04b49c246cbf88bd3338ab7e9a4
Justin Dotson <ileicathat@gmail.com>
2023-06-12 23:25:31 -0700

    Init up

--- Seattle_301d8_Ops_Challenge_Class_10.py
+++ Seattle_301d8_Ops_Challenge_Class_10.py
@@ -20,3 +20,29 @@
 # f = open("demofile.txt", "r")
 # print(f.readline())
 # 
+
+# Start Code
+
+# Create a new text file
+file_path = "example.txt"
+file = open(file_path, "w")
+
+# Append three lines to the file
+lines = ["First line", "Second line", "Third line"]
+for line in lines:
+    file.write(line + "\n")
+
+# Close the file
+file.close()
+
+# Read the first line and print it
+file = open(file_path, "r")
+first_line = file.readline().rstrip()
+print("First line:", first_line)
+
+# Close the file
+file.close()
+
+# Delete the file
+import os
+os.remove(file_path)
