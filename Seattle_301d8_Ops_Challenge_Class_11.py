8a2b4ff9d5405d8d56a90bb8b5f1483c0387ce2f
Justin Dotson <ileicathat@gmail.com>
2023-06-14 00:26:14 -0700

    init up

---
+++ Seattle_301d8_Ops_Challenge_Class_11.py
@@ -0,0 +1,18 @@
+# Ops Challenge: Psutil
+# Overview
+# Python is known for its versatile and diverse selection of libraries. Libraries are installable toolkits that add new functions to the language on your system. Today you will use Psutil to fetch system information.
+
+# Objectives
+# Install Psutil.
+
+# Create a Python script that fetches this information using Psutil:
+
+# Time spent by normal processes executing in user mode
+# Time spent by processes executing in kernel mode
+# Time when system was idle
+# Time spent by priority processes executing in user mode
+# Time spent waiting for I/O to complete.
+# Time spent for servicing hardware interrupts
+# Time spent for servicing software interrupts
+# Time spent by other operating systems running in a virtualized environment
+# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
