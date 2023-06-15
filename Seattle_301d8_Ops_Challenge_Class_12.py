13192ba43d790fe6b26292ee398dad72b220e8c7
Justin Dotson <ileicathat@gmail.com>
2023-06-14 23:56:01 -0700

    update

--- Seattle_301d8_Ops_Challenge_Class_12.py
+++ Seattle_301d8_Ops_Challenge_Class_12.py
@@ -43,12 +43,57 @@
 
 # First you'll need to install the requests library to your system
 # Next, like any library, import it into your Python script
-import requests
+#import requests
 
 # Assign a requests.get function to a variable. Be sure to pass a parameter (the URL) into the function for it to work.
 response = requests.get('https://api.github.com')
 
 # Print the status code obtained by the function using [var name].status code
-print(response.status_code)
+#print(response.status_code)
 
 # This will return status code 200. Look it up at https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+
+# START CODE
+
+#!/usr/bin/env python3
+
+
+import requests
+
+# Prompt the user for destination URL
+url = input("Enter the destination URL including https://: ")
+
+# Prompt the user to select HTTP Method
+http_method = input("Select an HTTP Method (GET, POST, PUT, DELETE, HEAD, PATCH, OPTIONS): ")
+
+# Print the request information
+print(f"Sending {http_method} request to: {url}")
+
+# Ask for user confirmation
+confirmation = input("Press 'Y' to confirm and proceed: ")
+
+if confirmation.lower() != 'y':
+    print("Request canceled.")
+else:
+    # Perform the request
+    if http_method.upper() == 'GET':
+        response = requests.get(url)
+    elif http_method.upper() == 'POST':
+        response = requests.post(url)
+    elif http_method.upper() == 'PUT':
+        response = requests.put(url)
+    elif http_method.upper() == 'DELETE':
+        response = requests.delete(url)
+    elif http_method.upper() == 'HEAD':
+        response = requests.head(url)
+    elif http_method.upper() == 'PATCH':
+        response = requests.patch(url)
+    elif http_method.upper() == 'OPTIONS':
+        response = requests.options(url)
+    else:
+        print("Invalid HTTP Method.")
+        exit()
+
+    # Print the response status code
+    print(f"Response Status Code: {response.status_code}")
