2f3754f1ef97c1d4926e704ab43b4e355f616a67
Justin Dotson <ileicathat@gmail.com>
2023-06-15 21:41:19 -0700

    init up

---
+++ Seattle_301d8_Ops_Challenge_Class_13.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# seattle-ops-301d8
+# Ops Challenge:Powershell AD Automation
+# Demo Code:
+
+#   New-ADUser -Name "ChewDavid" -OtherAttributes @{'title'="director";'mail'="chewdavid@fabrikam.com"}
